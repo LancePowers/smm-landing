771e34055dc2b809137abd2eb0aaa377a08b1bdd
Lance <Lance@thelpstory.com>
2016-09-08 08:32:26 -0600

    Slider

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  console.log('slick')
+
+  var item_length = $('.ss > div').length - 1;
+  var slider = $('.ss').slick({
+    dots: true,
+    speed: 700,
+    autoplay:true,
+    autoplaySpeed: 3000,
+    infinite: false
+  });
+
+  // On before slide change
+  slider.on('afterChange', function(event, slick, currentSlide, nextSlide){
+
+     //check the length of total items in .slide container
+   //if that number is the same with the number of the last slider
+   //Then pause the slider
+   if( item_length === slider.slick('slickCurrentSlide') ){
+    slider.slickPause();
+    //  slider.slickSetOption("autoplay",false,false)
+   };
+  });
+
+});
